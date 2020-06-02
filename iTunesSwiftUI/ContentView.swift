e00713d8d7dc1ab6783a44ce7d2bea4fd0c70f1f
Cody Morley <codymorleyinc@gmail.com>
2020-06-02 11:14:58 -0700

    finished up a sample swiftUI app.

--- iTunesSwiftUI/ContentView.swift
+++ iTunesSwiftUI/ContentView.swift
@@ -10,15 +10,43 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var artistName = ""
+    @State var artistGenre = ""
+    
+    
     var body: some View {
         VStack() {
-            Text("Hello, World")
+            Text("Search for artists with iTunesAPI")
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            SearchBar(artistName: $artistName, artistGenre: $artistGenre)
+            TextField("Artist Name", text: $artistName)
+            TextField("Artist Genre", text: $artistGenre)
+            Text(artistName)
+                .font(.largeTitle)
+                .bold()
+                .multilineTextAlignment(.center)
+                .lineLimit(3)
+                .padding([.leading, .bottom, .trailing], 12)
+            HStack {
+                if !artistGenre.isEmpty {
+                Text("Artist Genre:")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                }
+                Text(artistGenre)
+                .font(.subheadline)
+                .foregroundColor(.primary)
+            }
+            
+            
+            Spacer()
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environment(\.colorScheme, .light)
     }
 }
